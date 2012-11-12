5e6eaac2b27b4c11cf440103870018a3f54239b5
Jordan Cristiano <jordan.cristi@gmail.com>
2012-11-12 02:37:53 -0500

    fixed linux build error

--- SizzlingStats/WebStatsHandler.cpp
+++ SizzlingStats/WebStatsHandler.cpp
@@ -165,7 +165,7 @@ void CWebStatsHandler::SendGameOverEventInternal(double flMatchDuration)
 				connection.AddHeader(temp);
 			}
 
-			connection.SetOption(CURLOPT_POSTFIELDS, "");
+			connection.SetOption(CURLOPT_POSTFIELDS, const_cast<char*>(""));
 			connection.SetUrl(GAMEOVER_URL);
 
 			CURLcode res = connection.Perform();
