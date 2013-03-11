1a8e0d3a02f1140a54a61d46bc814c27f1f04608
Jordan Cristiano <jordan.cristi@gmail.com>
2013-03-10 22:14:58 -0400

    [sizzstats] alpha v0.9.1.1

--- SizzlingStats/PluginDefines.h
+++ SizzlingStats/PluginDefines.h
@@ -19,7 +19,7 @@
 	#define PLUGIN_VERSION "0.8.3.7"
 	#define URL_BASE "http://dl.dropbox.com/u/45675887/permlinks/tf2plugins/SizzlingStats/"
 #else
-	#define PLUGIN_VERSION "0.9.1.0"
+	#define PLUGIN_VERSION "0.9.1.1"
 	#define URL_BASE "http://dl.dropbox.com/u/45675887/permlinks/tf2plugins/SizzlingStats/beta/"
 #endif
 //#define RELEASE_VERSION
