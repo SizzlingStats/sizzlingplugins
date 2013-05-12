729b57b4ca8a08f677b6916b1cece913b2313eb9
Jordan Cristiano <jordan.cristi@gmail.com>
2013-05-12 17:39:39 -0400

    commented out warning line

--- SizzlingStats/PlayerDataManager.cpp
+++ SizzlingStats/PlayerDataManager.cpp
@@ -66,7 +66,7 @@ bool CPlayerDataManager::InsertPlayer( engineContext_t &context, edict_t *pEdict
 	// normalize with a -1 for array indexing
 	int ent_index = context.pEngine->IndexOfEdict(pEdict) - 1;
 
-	uint32 account_id = pSteamID->GetAccountID();
+	//uint32 account_id = pSteamID->GetAccountID();
 	UtlHashFastHandle_t hHash = m_playerDataArchive.InvalidHandle(); //m_playerDataArchive.Find(account_id);
 
 	// if the player is not in the hash map, ie. not archived
