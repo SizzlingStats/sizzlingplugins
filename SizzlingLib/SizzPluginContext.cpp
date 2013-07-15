ccd4b60fe20111b067e72d9e70238763bf9e2d77
Jordan Cristiano <jordan.cristi@gmail.com>
2013-07-15 15:04:28 -0400

    added a bunch of plugin context functions

--- SizzlingLib/SizzPluginContext.cpp
+++ SizzlingLib/SizzPluginContext.cpp
@@ -14,12 +14,21 @@
 #include "eiface.h"
 #include "UserIdTracker.h"
 #include "edict.h"
-#include "steam\steamclientpublic.h"
+#include "steam/steamclientpublic.h"
+#include "ServerPluginHandler.h"
+#include "game/server/iplayerinfo.h"
+#include "igameevents.h"
+#include "ThreadCallQueue.h"
 
 CSizzPluginContext::CSizzPluginContext():
 	m_pEngine(nullptr),
+	m_pPlayerInfoManager(nullptr),
+	m_pPluginManager(nullptr),
+	m_pGameEventManager(nullptr),
+	m_pServerGameDLL(nullptr),
 	m_pGlobals(nullptr),
 	m_pUserIDTracker(new CUserIDTracker()),
+	m_pCallQueue(new CTSCallQueue()),
 	m_tickcount(0),
 	m_flTime(0.0f)
 {
@@ -27,13 +36,23 @@ CSizzPluginContext::CSizzPluginContext():
 
 CSizzPluginContext::~CSizzPluginContext()
 {
+	delete m_pCallQueue;
 	delete m_pUserIDTracker;
 }
 
 void CSizzPluginContext::Initialize( const plugin_context_init_t &init )
 {
 	m_pEngine = init.pEngine;
-	m_pGlobals = init.pGlobals;
+	m_pPlayerInfoManager = init.pPlayerInfoManager;
+	m_pPluginManager = reinterpret_cast<CServerPlugin*>(init.pHelpers);
+	m_pGameEventManager = init.pGameEventManager;
+	m_pServerGameDLL = init.pServerGameDLL;
+
+	if (m_pPlayerInfoManager)
+	{
+		m_pGlobals = m_pPlayerInfoManager->GetGlobalVars();
+	}
+
 	m_pUserIDTracker->Reset();
 }
 
@@ -47,7 +66,7 @@ int CSizzPluginContext::UserIDFromEntIndex( int ent_index )
 	return -1;
 }
 
-int CSizzPluginContext::EntIndexFromUserID( int userid )
+int CSizzPluginContext::EntIndexFromUserID( int userid ) const
 {
 	int ent_index = m_pUserIDTracker->GetEntIndex(userid);
 	return ent_index;
@@ -77,6 +96,96 @@ int CSizzPluginContext::GetCurrentTick() const
 float CSizzPluginContext::GetTime() const
 {
 	return m_flTime;
+}
+
+int CSizzPluginContext::GetPluginIndex( const IServerPluginCallbacks *pPlugin ) const
+{
+	if (pPlugin)
+	{
+		int num_plugins = m_pPluginManager->m_plugins.Count();
+		for ( int i = 0; i < num_plugins; ++i )
+		{
+			CPlugin *plugin = m_pPluginManager->m_plugins[i];
+			if (plugin && (plugin->m_pPlugin == pPlugin))
+			{
+				return i;
+			}
+		}
+	}
+	return -1;
+}
+
+int CSizzPluginContext::GetPluginIndex( const char *pszDescriptionPart ) const
+{
+	if (pszDescriptionPart)
+	{
+		int num_plugins = m_pPluginManager->m_plugins.Count();
+		for ( int i = 0; i < num_plugins; ++i )
+		{
+			CPlugin *pPlugin = m_pPluginManager->m_plugins[i];
+			if (pPlugin && V_strstr(pPlugin->m_szName, pszDescriptionPart))
+			{
+				return i;
+			}
+		}
+	}
+	return -1;
+}
+
+void CSizzPluginContext::ServerCommand( const char *command )
+{
+	m_pEngine->ServerCommand(command);
+}
+
+void CSizzPluginContext::ServerExecute()
+{
+	m_pEngine->ServerExecute();
+}
+
+void CSizzPluginContext::LogPrint( const char *msg )
+{
+	m_pEngine->LogPrint(msg);
+}
+
+bool CSizzPluginContext::IsPaused()
+{
+	return m_pEngine->IsPaused();
+}
+
+bool CSizzPluginContext::AddListener( IGameEventListener2 *listener, const char *name, bool bServerSide )
+{
+	return m_pGameEventManager->AddListener(listener, name, bServerSide);
+}
+
+void CSizzPluginContext::RemoveListener( IGameEventListener2 *listener )
+{
+	m_pGameEventManager->RemoveListener(listener);
+}
+
+IPlayerInfo *CSizzPluginContext::GetPlayerInfo( int ent_index )
+{
+	if ((1 <= ent_index) && (ent_index < m_pGlobals->maxClients))
+	{
+		edict_t *pEdict = m_pEngine->PEntityOfEntIndex(ent_index);
+		if (pEdict && !pEdict->IsFree())
+		{
+			return m_pPlayerInfoManager->GetPlayerInfo(pEdict);
+		}
+	}
+	return nullptr;
+}
+
+void CSizzPluginContext::EnqueueGameFrameFunctor( CFunctor *pFunctor )
+{
+	if (pFunctor)
+	{
+		m_pCallQueue->EnqueueFunctor(pFunctor);
+	}
+}
+
+ServerClass *CSizzPluginContext::GetAllServerClasses()
+{
+	return m_pServerGameDLL->GetAllServerClasses();
 }
 
 void CSizzPluginContext::ClientActive( const edict_t *pEdict )
@@ -115,4 +224,6 @@ void CSizzPluginContext::GameFrame( bool simulating )
 {
 	m_tickcount = m_pGlobals->tickcount;
 	m_flTime = m_pGlobals->realtime;
+
+	m_pCallQueue->callQueueGameFrame();
 }
--- SizzlingLib/include/SizzPluginContext.h
+++ SizzlingLib/include/SizzPluginContext.h
@@ -18,11 +18,25 @@ class IVEngineServer;
 class CGlobalVars;
 class CUserIDTracker;
 struct edict_t;
+class CServerPlugin;
+class IServerPluginHelpers;
+class IGameEventManager2;
+class IPlayerInfoManager;
+class IServerPluginCallbacks;
+class IGameEventListener2;
+class IPlayerInfo;
+class CTSCallQueue;
+class CFunctor;
+class ServerClass;
+class IServerGameDLL;
 
 typedef struct plugin_context_init_s
 {
 	IVEngineServer *pEngine;
-	CGlobalVars *pGlobals;
+	IPlayerInfoManager *pPlayerInfoManager;
+	IServerPluginHelpers *pHelpers;
+	IGameEventManager2 *pGameEventManager;
+	IServerGameDLL *pServerGameDLL;
 } plugin_context_init_t;
 
 class CSizzPluginContext
@@ -38,7 +52,7 @@ public:
 	int UserIDFromEntIndex( int ent_index );
 
 	// returns -1 if userid is invalid
-	int EntIndexFromUserID( int userid );
+	int EntIndexFromUserID( int userid ) const;
 
 	// returns -1 on error
 	int SteamIDFromUserID( int userid );
@@ -52,6 +66,44 @@ public:
 	// the current time (per frame incremented)
 	float GetTime() const;
 
+	// returns the index of the plugin attached 
+	// to the passed in plugin callbacks
+	int GetPluginIndex( const IServerPluginCallbacks *pPlugin ) const;
+
+	// returns the index of the first plugin whos 
+	// description string contains the passed 
+	// in description string
+	int GetPluginIndex( const char *pszDescriptionPart ) const;
+
+	// Issue a command to the command parser as if it was typed at the server console.
+	void ServerCommand( const char *command );
+
+	// Execute any commands currently in the command parser immediately (instead of once per frame)
+	void ServerExecute();
+
+	// Print a message to the server log file
+	void LogPrint( const char *msg );
+
+	// Is the game paused?
+	bool IsPaused();
+
+	// adds a listener for a particular event
+	bool AddListener( IGameEventListener2 *listener, const char *name, bool bServerSide );
+	
+	// removes a listener
+	void RemoveListener( IGameEventListener2 *listener );
+
+	// returns the player info pointer for the 
+	// given player ent index
+	IPlayerInfo *GetPlayerInfo( int ent_index );
+
+	// adds a functor to the call queue to be executed 
+	// on a future game frame (thread safe with the game logic)
+	void EnqueueGameFrameFunctor( CFunctor *pFunctor );
+
+	// returns a pointer to the server class list
+	ServerClass *GetAllServerClasses();
+
 protected:
 	void ClientActive( const edict_t *pEdict );
 	void ClientDisconnect( const edict_t *pEdict );
@@ -59,9 +111,14 @@ protected:
 
 private:
 	IVEngineServer *m_pEngine;
+	IPlayerInfoManager *m_pPlayerInfoManager;
+	CServerPlugin *m_pPluginManager;
+	IGameEventManager2 *m_pGameEventManager;
+	IServerGameDLL *m_pServerGameDLL;
 	CGlobalVars *m_pGlobals;
-	CUserIDTracker *m_pUserIDTracker;
 
+	CUserIDTracker *m_pUserIDTracker;
+	CTSCallQueue *m_pCallQueue;
 	int m_tickcount;
 	float m_flTime;
 };
