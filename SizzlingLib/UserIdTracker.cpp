ea3f5bd40e8ba3a4a7d15301713acd21edbf2a45
Jordan Cristiano <jordan.cristi@gmail.com>
2013-07-15 23:34:09 -0400

    removed old userid tracker completely

--- SizzlingLib/UserIdTracker.cpp
+++ SizzlingLib/UserIdTracker.cpp
@@ -13,53 +13,7 @@
 // Filename: UserIdTracker.cpp
 ////////////////////////////////////////////////////////////////////////////////
 #include "UserIdTracker.h"
-
-#include "eiface.h"
-
-// Interfaces from the engine
-extern IVEngineServer			*pEngine;
-
-static UserIdTracker gUserIdTracker;
-UserIdTracker *g_pUserIdTracker = &gUserIdTracker;
-
-UserIdTracker::UserIdTracker()
-{
-	// increases binary size by the array size for some reason
-	//Load();
-}
-
-UserIdTracker::~UserIdTracker()
-{	
-}
-
-void UserIdTracker::Load()
-{
-	Reset();
-}
-
-void UserIdTracker::Reset()
-{
-	memset( m_entIndexTable, -1, 65536 );
-}
-
-int UserIdTracker::ClientActive( edict_t *pEdict )
-{
-	int userid = pEngine->GetPlayerUserId( pEdict );
-	Assert(userid != -1);
-	m_entIndexTable[userid] = pEngine->IndexOfEdict( pEdict );
-	return static_cast<int>( m_entIndexTable[userid] );
-}
-
-void UserIdTracker::ClientDisconnect( edict_t *pEdict )
-{
-	int userid = pEngine->GetPlayerUserId( pEdict );
-	m_entIndexTable[userid] = -1;
-}
-
-int UserIdTracker::GetEntIndex( int userid ) const
-{
-	return static_cast<int>( m_entIndexTable[userid] );
-}
+#include "strtools.h"
 
 int CUserIDTracker::GetEntIndex( int userid ) const
 {
--- SizzlingLib/include/UserIdTracker.h
+++ SizzlingLib/include/UserIdTracker.h
@@ -15,26 +15,6 @@
 #ifndef USER_ID_TRACKER_H
 #define USER_ID_TRACKER_H
 
-struct edict_t;
-
-class UserIdTracker
-{
-public:
-	UserIdTracker(void);
-	~UserIdTracker(void);
-
-	void		Load();
-	void		Reset();
-	int 		ClientActive( edict_t *pEdict );
-	void 		ClientDisconnect( edict_t *pEdict );
-	int 		GetEntIndex( int userid ) const;
-
-private:
-	char m_entIndexTable[65536];
-};
-
-extern UserIdTracker *g_pUserIdTracker;
-
 class CUserIDTracker
 {
 	friend class CSizzPluginContext;
