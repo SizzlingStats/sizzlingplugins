22ba5c5fd19629ab53c2473dddac4ddf7466fac7
Jordan Cristiano <jordan.cristi@gmail.com>
2013-07-18 16:03:07 -0400

    added usermessage helper functions class

---
+++ SizzlingLib/UserMessageHelpers.cpp
@@ -0,0 +1,129 @@
+
+/*========
+        This file is part of SizzlingPlugins.
+
+    Copyright (c) 2010-2013, Jordan Cristiano.
+    This file is subject to the terms and conditions 
+    defined in the file 'LICENSE', which is part of this
+    source code package.
+    
+*/
+
+#include "UserMessageHelpers.h"
+#include "SizzPluginContext.h"
+#include "SRecipientFilter.h"
+#include "MRecipientFilter.h"
+
+CUserMessageHelpers::CUserMessageHelpers( CSizzPluginContext &context ):
+	m_context(context)
+{
+}
+
+void CUserMessageHelpers::SingleUserChatMessage( int ent_index, const char *format, ... )
+{
+	va_list args;
+	va_start(args, format);
+	SRecipientFilter_new filter(m_context, ent_index);
+	m_context.ChatMessageArg(&filter, format, args);
+	va_end(args);
+}
+
+void CUserMessageHelpers::SingleUserChatMessageArg( int ent_index, const char *format, va_list args )
+{
+	SRecipientFilter_new filter(m_context, ent_index);
+	m_context.ChatMessageArg(&filter, format, args);
+}
+
+void CUserMessageHelpers::AllUserChatMessage( const char *format, ... )
+{
+	va_list args;
+	va_start(args, format);
+	MRecipientFilter_new filter(m_context);
+	filter.AddAllPlayers();
+	m_context.ChatMessageArg(&filter, format, args);
+	va_end(args);
+}
+
+void CUserMessageHelpers::AllUserChatMessageArg( const char *format, va_list args )
+{
+	MRecipientFilter_new filter(m_context);
+	filter.AddAllPlayers();
+	m_context.ChatMessageArg(&filter, format, args);
+}
+
+void CUserMessageHelpers::SingleUserHudResetMessage( int ent_index )
+{
+	SRecipientFilter_new filter(m_context, ent_index);
+	m_context.HudResetMessage(&filter);
+}
+
+void CUserMessageHelpers::AllUserHudResetMessage()
+{
+	MRecipientFilter_new filter(m_context);
+	filter.AddAllPlayers();
+	m_context.HudResetMessage(&filter);
+}
+
+void CUserMessageHelpers::SingleUserHudMessage( int ent_index, const hud_msg_cfg_t &cfg, const char *format, ... )
+{
+	va_list args;
+	va_start(args, format);
+	SRecipientFilter_new filter(m_context, ent_index);
+	m_context.HudMessageArg(&filter, cfg, format, args);
+	va_end(args);
+}
+
+void CUserMessageHelpers::SingleUserHudMessageArg( int ent_index, const hud_msg_cfg_t &cfg, const char *format, va_list args )
+{
+	SRecipientFilter_new filter(m_context, ent_index);
+	m_context.HudMessageArg(&filter, cfg, format, args);
+}
+
+void CUserMessageHelpers::AllUserHudMessage( const hud_msg_cfg_t &cfg, const char *format, ... )
+{
+	va_list args;
+	va_start(args, format);
+	MRecipientFilter_new filter(m_context);
+	filter.AddAllPlayers();
+	m_context.HudMessageArg(&filter, cfg, format, args);
+	va_end(args);
+}
+
+void CUserMessageHelpers::AllUserHudMessageArg( const hud_msg_cfg_t &cfg, const char *format, va_list args )
+{
+	MRecipientFilter_new filter(m_context);
+	filter.AddAllPlayers();
+	m_context.HudMessageArg(&filter, cfg, format, args);
+}
+
+void CUserMessageHelpers::SingleUserHudHintMessage( int ent_index, const char *format, ... )
+{
+	va_list args;
+	va_start(args, format);
+	SRecipientFilter_new filter(m_context, ent_index);
+	m_context.HudHintMessageArg(&filter, format, args);
+	va_end(args);
+}
+
+void CUserMessageHelpers::SingleUserHudHintMessageArg( int ent_index, const char *format, va_list args )
+{
+	SRecipientFilter_new filter(m_context, ent_index);
+	m_context.HudHintMessageArg(&filter, format, args);
+}
+
+void CUserMessageHelpers::AllUserHudHintMessage( const char *format, ... )
+{
+	va_list args;
+	va_start(args, format);
+	MRecipientFilter_new filter(m_context);
+	filter.AddAllPlayers();
+	m_context.HudHintMessageArg(&filter, format, args);
+	va_end(args);
+}
+
+void CUserMessageHelpers::AllUserHudHintMessageArg( const char *format, va_list args )
+{
+	MRecipientFilter_new filter(m_context);
+	filter.AddAllPlayers();
+	m_context.HudHintMessageArg(&filter, format, args);
+}
---
+++ SizzlingLib/include/UserMessageHelpers.h
@@ -0,0 +1,50 @@
+
+/*========
+        This file is part of SizzlingPlugins.
+
+    Copyright (c) 2010-2013, Jordan Cristiano.
+    This file is subject to the terms and conditions 
+    defined in the file 'LICENSE', which is part of this
+    source code package.
+    
+*/
+
+#ifndef USER_MESSAGE_HELPERS
+#define USER_MESSAGE_HELPERS
+
+#include <stdarg.h>
+
+class CSizzPluginContext;
+typedef struct hud_msg_cfg_s hud_msg_cfg_t;
+
+class CUserMessageHelpers
+{
+public:
+	CUserMessageHelpers( CSizzPluginContext &context );
+
+	void SingleUserChatMessage( int ent_index, const char *format, ... );
+	void SingleUserChatMessageArg( int ent_index, const char *format, va_list args );
+
+	void AllUserChatMessage( const char *format, ... );
+	void AllUserChatMessageArg( const char *format, va_list args );
+
+	void SingleUserHudResetMessage( int ent_index );
+	void AllUserHudResetMessage();
+
+	void SingleUserHudMessage( int ent_index, const hud_msg_cfg_t &cfg, const char *format, ... );
+	void SingleUserHudMessageArg( int ent_index, const hud_msg_cfg_t &cfg, const char *format, va_list args );
+
+	void AllUserHudMessage( const hud_msg_cfg_t &cfg, const char *format, ... );
+	void AllUserHudMessageArg( const hud_msg_cfg_t &cfg, const char *format, va_list args );
+
+	void SingleUserHudHintMessage( int ent_index, const char *format, ... );
+	void SingleUserHudHintMessageArg( int ent_index, const char *format, va_list args );
+
+	void AllUserHudHintMessage( const char *format, ... );
+	void AllUserHudHintMessageArg( const char *format, va_list args );
+
+private:
+	CSizzPluginContext &m_context;
+};
+
+#endif // USER_MESSAGE_HELPERS
