de1a56d8c2b1ecb210e0a3af7baadd5f925ade56
Jordan Cristiano <jordan.cristi@gmail.com>
2014-02-15 00:57:08 -0500

    sizzlingstats v0.9.4.0a
    - stv demo recording and uploading
    - misc bug fixes
    - console script fix

--- sizzlingstats/PluginDefines.h
+++ sizzlingstats/PluginDefines.h
@@ -18,7 +18,7 @@
 //#define LOG_STATS
 //#define USE_STAGING_URLS
 
-#define PLUGIN_VERSION "0.9.3.2"
+#define PLUGIN_VERSION "0.9.4.0"
 #define URL_BASE "https://bitbucket.org/jcristiano/sizzlingplugins/downloads/"
 
 #ifdef _WIN32
