d40fecf2151ced60dc9ab3171b03bb679b1d69b3
Jordan Cristiano <jordan.cristi@gmail.com>
2015-05-31 22:18:03 -0400

    Replaced NULL define with cstddef include

--- sizzlinglib/include/HtmlGenerator.h
+++ sizzlinglib/include/HtmlGenerator.h
@@ -15,13 +15,7 @@
 #ifndef HTML_GENERATOR_H
 #define HTML_GENERATOR_H
 
-#ifndef NULL
-#ifdef __cplusplus
-#define NULL 0
-#else
-#define NULL ((void *)0)
-#endif
-#endif
+#include <cstddef>
 
 class CUtlBuffer;
 
