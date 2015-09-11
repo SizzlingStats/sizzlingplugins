40611d709e41b2126ea5a151ec702fe94cf4cc33
Jordan Cristiano <jordan.cristi@gmail.com>
2015-09-10 20:51:14 -0400

    SizzlingStats v0.9.4.3

--- sizzlingstats/PluginDefines.h
+++ sizzlingstats/PluginDefines.h
@@ -17,7 +17,7 @@
 //#define LOG_STATS
 //#define USE_STAGING_URLS
 
-#define PLUGIN_VERSION "0.9.4.2"
+#define PLUGIN_VERSION "0.9.4.3"
 #define URL_BASE "https://bitbucket.org/jcristiano/sizzlingplugins/downloads/"
 
 #ifdef _WIN32
